24e8acb32b646e06430631e514721f2cdd9fd44a
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-12-26 09:05:48 -0700

    Use go:generate + stringer; Fixes #2

--- doc.go
+++ doc.go
@@ -1,6 +1,7 @@
 // Copyright 2014 The Azul3D Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+//go:generate stringer -type=State,Key -output=stringers.go
 
 // Package keyboard implements various keyboard related data types.
 package keyboard // import "azul3d.org/keyboard.v2-dev"
--- state.go
+++ state.go
@@ -4,10 +4,6 @@
 
 package keyboard
 
-import (
-	"fmt"
-)
-
 type State uint8
 
 const (
@@ -17,16 +13,3 @@ const (
 	On           = Down // the on/active state (for lock keys; Caps Lock; Num Lock; etc..)
 	Off          = Up   // the off/inactive state (for lock keys; Caps Lock; Num Lock; etc..)
 )
-
-// String returns a string representation of this keyboard key state.
-func (s State) String() string {
-	switch s {
-	case InvalidState:
-		return "InvalidState"
-	case Down:
-		return "Down"
-	case Up:
-		return "Up"
-	}
-	return fmt.Sprintf("State(%d)", s)
-}
