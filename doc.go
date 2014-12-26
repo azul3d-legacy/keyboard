7f6af0aba5c4a00eb8e415c1306685eb64b86c4f
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-12-26 08:57:34 -0700

    Move package docs to a seperate file.

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2014 The Azul3D Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package keyboard implements various keyboard related data types.
+package keyboard // import "azul3d.org/keyboard.v2-dev"
--- keys.go
+++ keys.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package keyboard implements various keyboard related data types.
 package keyboard
 
 import (
